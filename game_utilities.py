7ae5216b25aa250148949d035b161e997ef937c2
Adetola Adewodu <adetola.adewodu@gmail.com>
2017-07-28 14:05:10 -0400

    Moved Balls and Lines to game_utilities

--- game_utilities.py
+++ game_utilities.py
@@ -9,7 +9,8 @@ BLUE = (0, 0, 255)
 YELLOW = (255,255,0)
 PURPLE = (75,0,130)
 
-class Block(pygame.sprite.Sprite):
+# Used to create a thick line
+class Line(pygame.sprite.Sprite):
     """
     This class represents the ball
     It derives from the "Sprite" class in Pygame
@@ -22,7 +23,7 @@ class Block(pygame.sprite.Sprite):
 
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width, height])
+        self.image = pygame.Surface([width*3, height*3])
         self.image.fill(color)
 
         # Fetch the rectangle object that has the dimensions of the image
@@ -30,6 +31,35 @@ class Block(pygame.sprite.Sprite):
         # Update the position of this object by setting the values
         # of rect.x and rect.y
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = x * 3
+        self.rect.y = y * 3
+
+
+class Ball(pygame.sprite.Sprite):
+    """
+    This class represents the ball
+    It derives from the "Sprite" class in Pygame
+    """
+    def __init__(self, color, width, height, block_id, signature):
+        """ Constructor. Pass in the color of the block,
+        and its x and y position. """
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([width*3, height*3])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+
+        # Set the block id and signature to be used later
+        self.block_id = block_id
+        self.signature = signature
+        # Fetch the rectangle object that has the dimensions of the image
+        # image.
+        # Update the position of this object by setting the values
+        # of rect.x and rect.y
+        self.rect = self.image.get_rect()
+        pygame.draw.ellipse(self.image,color, [0, 0, width*3, height*3])
 
--- moving_sprites_from_serial.py
+++ moving_sprites_from_serial.py
@@ -7,15 +7,10 @@ import ujson as json
 import serial
 from pygame.transform import scale
 from pygame.locals import *
+from colors import *
+from score import *
+from game_utilities import *
 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255,255,0)
-PURPLE = (75,0,130)
 
 # Barriers
 LEFT_BARRIER = 43
@@ -60,59 +55,7 @@ for color in colors:
 html_file.write("""</body>
 </html>""")
 html_file.close()
-# Used to create a thick line
-class Line(pygame.sprite.Sprite):
-    """
-    This class represents the ball
-    It derives from the "Sprite" class in Pygame
-    """
-    def __init__(self, color, width, height, x, y):
-        """ Constructor. Pass in the color of the block,
-        and its x and y position. """
-        # Call the parent class (Sprite) constructor
-        super().__init__()
 
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width*3, height*3])
-        self.image.fill(color)
-
-        # Fetch the rectangle object that has the dimensions of the image
-        # image.
-        # Update the position of this object by setting the values
-        # of rect.x and rect.y
-        self.rect = self.image.get_rect()
-        self.rect.x = x * 3
-        self.rect.y = y * 3
-
-
-class Ball(pygame.sprite.Sprite):
-    """
-    This class represents the ball
-    It derives from the "Sprite" class in Pygame
-    """
-    def __init__(self, color, width, height, block_id, signature):
-        """ Constructor. Pass in the color of the block,
-        and its x and y position. """
-        # Call the parent class (Sprite) constructor
-        super().__init__()
-
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width*3, height*3])
-        self.image.fill(BLACK)
-        self.image.set_colorkey(BLACK)
-
-
-        # Set the block id and signature to be used later
-        self.block_id = block_id
-        self.signature = signature
-        # Fetch the rectangle object that has the dimensions of the image
-        # image.
-        # Update the position of this object by setting the values
-        # of rect.x and rect.y
-        self.rect = self.image.get_rect()
-        pygame.draw.ellipse(self.image,color, [0, 0, width*3, height*3])
 
 
 # Initialize Pygame
